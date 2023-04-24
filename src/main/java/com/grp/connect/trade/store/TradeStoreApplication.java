0fc6a03d62292f849f19b3ddea9332851ff9258c
Gaurav Rajapurkar <connect2grp@gmail.com>
2023-04-25 03:21:37 +0530

    Initial commit

--- src/main/java/com/grp/connect/trade/store/TradeStoreApplication.java
+++ src/main/java/com/grp/connect/trade/store/TradeStoreApplication.java
@@ -2,6 +2,7 @@ package com.grp.connect.trade.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class TradeStoreApplication {
 
